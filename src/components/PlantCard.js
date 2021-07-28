5d88287bec8d8beb7914a878f006e8a653da1d31
Alieu Baldeh <alieu.dev@gmail.com>
2021-07-28 16:50:29 -0400

    finished core deliverables

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
-function PlantCard() {
+function PlantCard({name, image, price}) {
+  const [stockStatus, setStockStatus] = useState(true)
+
+  function handleClick() {
+    setStockStatus(!stockStatus)
+  }
+  const btnClass = stockStatus? "primary" : ""
   return (
     <li className="card">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
-      ) : (
-        <button>Out of Stock</button>
-      )}
+      <img src={image} alt={name} />
+      <h4>{name}</h4>
+      <p>Price: {price}</p>
+        { <button onClick={handleClick}
+          className={btnClass}>
+            {stockStatus? "In Stock" : "Out of Stock"}
+          </button>
+        }
     </li>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
-function Search() {
+function Search({onSearchData}) {
+  const [searchValue, setSearchValue] = useState("");
+
+  onSearchData(searchValue)
+
   return (
     <div className="searchbar">
-      <label htmlFor="search">Search Plants:</label>
-      <input
-        type="text"
-        id="search"
-        placeholder="Type a name to search..."
-        onChange={(e) => console.log("Searching...")}
-      />
+        <label htmlFor="search">Search Plants:</label>
+        <form>
+          <input
+            type="text"
+            id="search"
+            placeholder="Type a name to search..."
+            onChange={(event) => setSearchValue(event.target.value)}
+          />
+          </form>
+      
     </div>
   );
 }
